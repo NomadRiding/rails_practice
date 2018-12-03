042a9ab9a67040e21ec46248662722ca0107786d
nomadriding <fundora.a@gmail.com>
2018-12-03 14:45:55 -0500

    people param

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,10 @@ class PeopleController < ApplicationController
 
     def index
         @people = Person.all
+        @people = @people.where(name: params[:name]) if params[:name].present?
+        @people = @people.where(mood: params[:mood]) if params[:mood].present?
+        @people = @people.where("age >= ?", params[:min_age].to_i) if params[:min_age].present?
+        @people = @people.where("age <= ?", params[:max_age].to_i) if params[:max_age].present?
     end
 
 end
